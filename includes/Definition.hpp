d0d9fd89a915181fb05615f66d0efa62b50a3157
rmerzak <rmerzak@student.1337.ma>
2023-04-08 14:45:56 +0000

    macro exception

---
+++ includes/Definition.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Definition.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmerzak <rmerzak@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 13:40:54 by rmerzak           #+#    #+#             */
+/*   Updated: 2023/04/08 14:42:47 by rmerzak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdexcept>
+
+
+/// VARIABLES
+
+#define MAX_CLIENTS 20
+
+#define THROW_EXCEPTION( str )       throw std::runtime_error( str )
+#define ERRCODE_EXCEPT( code ) THROW_EXCEPTION( std::strerror( code ) )
+
+#define ARGS_EXCEPTION  THROW_EXCEPTION( "Usage: ./ircserv <port> <password>" )
+#define PORT_EXCEPTION  THROW_EXCEPTION( "Must respect Port Range for Irc Server! [66660, 66669]" )
+#define PASSWORD_EXCEPTION     THROW_EXCEPTION( "password shoud be not be empty" )
+#define BIND_EXCEPTION THROW_EXCEPTION("Failed to bind socket to address and port")
+#define LISTEN_EXCEPTION THROW_EXCEPTION("Failed to start listening on socket.")
