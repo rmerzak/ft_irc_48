fb892b1f81302f61ab9ca3bfcd4cf9fa5ce147d6
rmerzak <rmerzak@student.1337.ma>
2023-04-13 23:34:04 +0000

    fix main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmerzak <rmerzak@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 12:18:55 by rmerzak           #+#    #+#             */
-/*   Updated: 2023/04/13 21:47:44 by rmerzak          ###   ########.fr       */
+/*   Updated: 2023/04/13 23:02:09 by rmerzak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int main(int argc,char **av) {
         if (argc != 3)
             ARGS_EXCEPTION;
         Server server(atoi(av[1]), av[2]);
-        server.initServer();
         server.runServer();
     }
     catch (std::exception &e) {
